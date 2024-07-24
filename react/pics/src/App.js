b6c359c56c278f38c1baeee71fcd42835525b00c
vvkulkarni <vikas.kulkarni@harman.com>
2024-07-24 16:38:11 +0530

    update react training

--- react/pics/src/App.js
+++ react/pics/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
+import ImageList from "./components/ImageList";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [imagesList, setImagesList] = useState([]);
+    const handleSubmit = async (term) => {
+        const result = await searchImages(term);
+        setImagesList(result);
+    };
+    
+    return (
+        <div>
+            <SearchBar onSubmit={handleSubmit} />
+            <ImageList images={imagesList}/>
+        </div>
+    );
+    
 }
 
 export default App;
